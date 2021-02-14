58c00d5a824dd26be862a1a16fedf77a39427a07
V-5000 <77503114+V-5000@users.noreply.github.com>
2021-02-14 07:29:35 +0000

    Update portal.py

--- portal.py
+++ portal.py
@@ -5,7 +5,16 @@ class display:
 	def setSize(x=300,y=250):
 		_root = tkinter.Tk()
 		_root.geometry(str(x)+"x"+str(y))
+		_root.title("Portal window")
+		_root.resizable(False,False)
+		
 		return _root
+
+	def setTitle(_root,title="Portal window"):
+		_root.title(title)
+
+	def setResizable(_root,x=True,y=True):
+		_root.resizable(x,y)
 
 	def update(_root):
 		_root.mainloop()
