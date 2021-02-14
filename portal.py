307eb365d9a46adc1a6fa7d9736c8a2d25dd85b1
V-5000 <77503114+V-5000@users.noreply.github.com>
2021-02-14 07:51:11 +0000

    Update portal.py

--- portal.py
+++ portal.py
@@ -2,7 +2,7 @@ import tkinter
 from tkinter import *
 
 class display:
-	def setSize(x=300,y=250):
+	def size(x=300,y=250):
 		_root = tkinter.Tk()
 		_root.geometry(str(x)+"x"+str(y))
 		_root.title("Portal window")
@@ -10,11 +10,23 @@ class display:
 		
 		return _root
 
-	def setTitle(_root,title="Portal window"):
+	def title(_root,title="Portal window"):
 		_root.title(title)
 
-	def setResizable(_root,x=True,y=True):
+	def resizable(_root,x=True,y=True):
 		_root.resizable(x,y)
 
 	def update(_root):
 		_root.mainloop()
+
+class surface:
+	def create(width,height):
+		_surface = Canvas()
+		return _surface
+
+	def pack(_surface):
+		_surface.pack(fill=None,expand=0)
+
+class draw:
+	def line(_surface,x,y,a,b):
+		_surface.create_line(x,y,a,b)
