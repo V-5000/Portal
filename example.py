73069a00593d30b93a6026eb85ed3655067b016d
V-5000 <77503114+V-5000@users.noreply.github.com>
2021-02-14 11:26:21 +0000

    Update example.py

--- example.py
+++ example.py
@@ -2,16 +2,21 @@ import portal,time
 
 window = portal.display.size(600,400)
 surf = portal.surface.create(portal.display.getwidth(window),portal.display.getheight(window))
+portal.surface.pack(surf)
+
+x=10
 
 while True:
 	f=time.time()
 	portal.draw.fill(surf)
-	portal.draw.line(surf,10,10,100,100,"#fff")
-	portal.surface.pack(surf)
 
-	portal.surface.forgot(surf)
+	portal.draw.line(surf,x,10,100,100,"#fff")
+
+	x+=1
+
 	portal.display.update(window)
-
+	portal.surface.forgot(surf)
+	
 	#keep this to avoid frame drops
 	time.sleep(0.0)
 	l=time.time()
